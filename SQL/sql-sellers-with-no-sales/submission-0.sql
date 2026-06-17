897d8edc8688312bf903d06340f7c32f3127f1b3
Zack <37523643+ZTW21@users.noreply.github.com>
2026-06-16 20:59:38 -0400

    Add: sql-sellers-with-no-sales - submission-0

---
+++ SQL/sql-sellers-with-no-sales/submission-0.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+select seller_name
+from seller
+where seller.seller_id not in (
+    select seller_id
+    from orders
+    where sale_date >= '2020-01-01' and sale_date <= '2020-12-31'
+)
+order by seller.seller_name ASC;
